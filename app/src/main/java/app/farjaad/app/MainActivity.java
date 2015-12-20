84553ede4ffaea927e58fe43f17bd59f0584e3aa
Farjaad <farjaad.rawasia@gmail.com>
2015-12-20 14:59:02 -0500

    added code so text updates if arts building is chosen

--- app/src/main/java/app/farjaad/app/MainActivity.java
+++ app/src/main/java/app/farjaad/app/MainActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class MainActivity extends Activity  {
 
 private Spinner spinner;
 private static final String[]buildings = {
+        "Choose Building",
         "Arts Lecture Hall (AL)",
         "B.C. Matthews Hall (BMH)",
         "Biology 1 (B1)",
@@ -60,45 +63,70 @@ private static final String[]buildings = {
         "University Club (UC)",
         "William M. Tatham Centre"
 };
+        private Button getStop;
+        private TextView stopInfo;
+
 
 @Override
 protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getStop = (Button)findViewById(R.id.getStop);
         spinner = (Spinner)findViewById(R.id.spinner);
+        stopInfo = (TextView)findViewById(R.id.stopInfo);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_spinner_item, buildings);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                        switch (position) {
+                                case 0:
+                                        stopInfo.setText("Choose Building Please!");
+                                        break;
+                                case 1:
+                                        // Whatever you want to happen when the first item gets selected
+                                        stopInfo.setText("Bus Stop Building and Number:\n"
+                                                + "\n " + "UW BC Matthews Hall- 1122\n");
+                                        break;
+                                case 2:
+                                        // Whatever you want to happen when the second item gets selected
+                                        break;
+                                case 3:
+                                        // Whatever you want to happen when the thrid item gets selected
+                                        break;
+
+                        }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parentView) {
+                        // your code here
+                }
+
+        });
 
         }
 
-public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+//public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+//
+//        switch (position) {
+//        case 0:
+//        // Whatever you want to happen when the first item gets selected
+//                stopInfo.setText("UW BC Matthews Hall- 1122\n");
+//        break;
+//        case 1:
+//        // Whatever you want to happen when the second item gets selected
+//        break;
+//        case 2:
+//        // Whatever you want to happen when the thrid item gets selected
+//        break;
+//
+//        }
+//        }
 
-        switch (position) {
-        case 0:
-        // Whatever you want to happen when the first item gets selected
-        break;
-        case 1:
-        // Whatever you want to happen when the second item gets selected
-        break;
-        case 2:
-        // Whatever you want to happen when the thrid item gets selected
-        break;
 
-        }
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-
-        @Override
-        public void onClick(View v) {
-
-        }
 }
